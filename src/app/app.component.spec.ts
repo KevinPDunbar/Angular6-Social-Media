3ba3b5308eb118a5311f28ff9bae8edfe887e129
Kevin <kevin.p.dunbar@gmail.com>
2018-10-24 20:36:04 +0100

    Update Project Files

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Angular-Socialdel'`, () => {
+  it(`should have as title 'Angular-Social'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Angular-Socialdel');
+    expect(app.title).toEqual('Angular-Social');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Angular-Socialdel!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Angular-Social!');
   });
 });
